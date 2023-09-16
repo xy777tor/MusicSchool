1b8ca1494f2aad2961ecc8e035a64b539984be22
xy777tor <khutornenko2001@gmail.com>
2023-09-16 16:45:03 +0300

    add float window with form to add new event by click on + button

--- MusicSchool.WebUI/Controllers/TimesheetController.cs
+++ MusicSchool.WebUI/Controllers/TimesheetController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicSchool.WebUI.Models;
 
 namespace MusicSchool.WebUI.Controllers;
 public class TimesheetController : Controller
 {
+    [HttpPost]
+    public string AddEvent(AddEventViewModel model)
+    {
+        return model.Title;
+    }
 }

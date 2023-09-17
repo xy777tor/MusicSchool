0b900da6a9917171f28a01c44dac9d01f9827764
xy777tor <khutornenko2001@gmail.com>
2023-09-18 02:08:12 +0300

    add not-implemented repository

---
+++ DataAccess/EventWindowRepository.cs
@@ -0,0 +1,17 @@
+﻿using MusicSchool.Core;
+using MusicSchool.Domain;
+
+namespace DataAccess;
+
+public class EventWindowRepository : IEventWindowRepository
+{
+    public void Add(EventWindow eventWindow)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<EventWindow> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
